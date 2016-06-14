6220080709c7585f3f68e12d74d44e60611b99bc
Austin Kwinta <austin.kwinta@portalanalytics.us>
2016-06-14 13:40:12 -0400

    Homebrew
    Allows the user to select on-hand ingredients and returns the list of possible drinks to be made

---
+++ app/containers/Homebrew.js
@@ -0,0 +1,47 @@
+var React = require('react');
+var styles = require('../styles');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
+var ingredientList = [];
+var selectedIngredients = [];
+
+var Homebrew = React.createClass({
+
+  getInitialState: function () {
+    return {
+      ingredientList: ['rum', 'tequila', 'vodka'],
+    };
+  },
+
+  showIngredientList: function () {
+    return this.state.ingredientList.map(function (ingredient) {
+      return <li className='checkbox'><label><input type='checkbox'></input>{ingredient}</label></li>;
+    });
+  },
+
+  filterDrinks: function () {
+    for (i = 0; i < drinkList.length - 1; i++) {
+      for (j = 0; j < selectedIngredients.length - 1; j++) {
+        if (drinkList[i].ingredients.includes(this.selectedIngredients[j])) {
+          availableDrinks.remove(drinkList[i]);
+        }
+      }
+
+    }
+  },
+
+  render: function () {
+    return (
+      <div>
+      <h1> Show us whatcha got</h1>
+      <h2> ingredients </h2>
+      <ul>
+      {this.showIngredientList()}
+      {this.filterDrinks()}
+      </ul>
+      </div>
+    );
+  },
+});
+
+module.exports = Homebrew;
