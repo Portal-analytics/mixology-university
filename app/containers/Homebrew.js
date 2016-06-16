c6b1afce02853662872eec03204986a6a4ee56d7
Austin Kwinta <austin.kwinta@portalanalytics.us>
2016-06-16 18:22:59 -0400

    Homebrew up and running
    Just make sure you click all the ingredients. Still a bit buggy.

--- app/containers/Homebrew.js
+++ app/containers/Homebrew.js
@@ -8,53 +8,159 @@ var selectedIngredients = [];
 var Homebrew = React.createClass({
 
   getInitialState: function () {
+
     return {
-      ingredientList: ['rum', 'tequila', 'vodka'],
+      ingredientList: ['Rum',
+                      'Coke',
+                      'Vodka',
+                      'Kahlua',
+                      'Cream',
+                      'Whiskey',
+                      '7-Up',
+                      'Tequila',
+                      'Red Bull',
+                      'Jägermeister',
+                      'Orange Juice',
+                      'Irish Cream',
+                      'Peach Schnapps',
+                      'Grenadine',
+                      'Water',
+                      'Everclear',
+                      'Jello',
+                      'Creme de Cacao',
+                      'Coco Lopez',
+                      'Amaretto',
+                      'Ice',
+                      'Nutmeg',
+                      'Whipped Cream',
+                      'Midori Melon Liqueur',
+                      'Sour Mix',
+                      'Bourbon',
+                      'Triple Sec',
+                      'Fruit Punch',
+                      'Pineapple Juice',
+                      'Creme de Menthe',
+                      'Creme de Cocoa',
+                      'Ice Cream',
+                      'Hpnotiq',
+                      'Hennessy Cognac',
+                      'Frangelico',
+                      'Southern Comfort',
+                      'Gin',
+                      'Lemon Juice',
+                      'Cranberry Juice',
+                      'Butterscotch Schnapps',
+                      'Blue Curacao',
+                      'Sour Apple Pucker',
+                      'Banana Liqueur',
+                      'Milk',
+                      'Lime Juice',
+                      'Apple Pucker Schnapps',
+                      'Big Red',
+                      'Sugar',
+                      'Mint',
+                      'Soda Water',
+                      'Guinness',
+                      'Bass Pale Ale',
+                      'Ginger Beer',
+                      'Goldschlager',
+                      'Chambord',
+                      'Beer',
+                      'Drambuie',
+                      'Scotch',
+                      'Skittles',
+                      'Hard Cider',
+                      'Salt',
+                      'Rumpleminze',
+                      'Grand Marnier',
+                      'Pineapple',
+                      'Ginger Ale',
+                      'Jameson',
+                      'Banana',
+                      'Pina Colada Mix',
+                      'Raspberry Liqueur',
+                      'Grapefruit Juice',
+                      'Cointreau',
+                      'Tangelo Juice',
+                      'Lemon',
+                      'Alize Cognac',
+                      'Cinnamon',
+                      'Brandy',
+                      'Lime',
+                      'cream of Coconut',
+                      'Cherry',
+                      'Chambord',
+                      'Lemonade',
+                      'Iced Tea',
+                      'Kool-Aid',
+                      'Benedictine',
+                      'Bailey',
+                      ],
+
+      drinkList: [],
     };
   },
 
-  addIngredient: function (ingredient) {
+  toggleIngredient: function (ingredient) {
+
     return function () {
-      selectedIngredients.push(ingredient);
-      console.log(selectedIngredients);
-    };
+
+      if (selectedIngredients.indexOf(ingredient) === -1) {
+        selectedIngredients.push(ingredient);
+        this.state.ingredientList.splice(this.state.ingredientList.indexOf(ingredient), 1);
+      } else {
+        this.state.ingredientList.push(ingredient);
+        selectedIngredients.splice(selectedIngredients.indexOf(ingredient), 1);
+      }
+
+      console.log(this.state.ingredientList.length);
+      console.log('Selected ingredients ' + selectedIngredients);
+
+    }.bind(this);
 
   },
 
-  onSubmit: function(){
-    for ( i = 0; i > ingredientList.length; i++) {
-      for ( j =0; j > selectedIngredients.length; j++ ) {
-        if (ingredientList[i].indexOf(selectedIngredients[j] > -1)){
-          ingredientList.splice(i, 1, '');
+  onSubmit: function () {
+    var finalDrinkList = [];
+    for (k = 0; k < this.state.drinkList.length; k++) {
+      finalDrinkList[k] = this.state.drinkList[k].ingredients;
+    }
+
+    console.log(finalDrinkList.length);
+    for (i = 0; i < this.state.ingredientList.length; i++) {
+      console.log('first loop');
+      for (j = 0; j < finalDrinkList.length; j++) {
+
+        if (finalDrinkList[j].indexOf(this.state.ingredientList[i]) > -1) {
+
+          this.state.drinkList.splice(j, 1, '');
         }
       }
     }
-    while (ingredientList.indexOf('') > -1) {
-      ingredientList.splice(ingredientList.indexOf(''), 1);
+
+    while (this.state.drinkList.indexOf('') > -1) {
+      this.state.drinkList.splice(this.state.drinkList.indexOf(''), 1);
+      console.log('snagged');
     }
-    console.log(ingredientList);
+
+    for (l = 0; l < this.state.drinkList.length; l++) {
+      console.log('Available drinks are : ');
+      console.log(this.state.drinkList[l].name + ' ' + this.state.drinkList[l].rating);
+
+    }
+
   },
 
   showIngredientList: function () {
+    this.state.ingredientList.sort();
     return this.state.ingredientList.map(function (ingredient, id) {
-      return <li key={id} onClick={this.addIngredient(ingredient)}>
-                <label>
+      return <li key={id} >
+                <label onClick={this.toggleIngredient(ingredient)}>
                 <input type='checkbox' value={ingredient}/>
                 {ingredient}
                 </label>
              </li>;
     }.bind(this));
-  },
-
-  filterDrinks: function () {
-    for (i = 0; i < drinkList.length - 1; i++) {
-      for (j = 0; j < selectedIngredients.length - 1; j++) {
-        if (drinkList[i].ingredients.includes(selectedIngredients[j])) {
-          availableDrinks.remove(drinkList[i]);
-        }
-      }
-
-    }
   },
 
   render: function () {
